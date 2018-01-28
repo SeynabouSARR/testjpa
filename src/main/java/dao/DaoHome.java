a1ae8dea4564c8f5bfd42f0fe22ffaadb20e55d5
Seynabou SARR <seynabousarr90@yahoo.fr>
2018-01-28 13:56:00 +0100

    DaoHome

--- src/main/java/dao/DaoHome.java
+++ src/main/java/dao/DaoHome.java
@@ -11,10 +11,12 @@ public class DaoHome {
 	
 	Dao managerObject;
 	EntityManager manager;
+	EntityTransaction transaction;
     
 	public DaoHome() {
 		managerObject = new Dao();
 		manager = managerObject.getManager();
+		transaction = manager.getTransaction();
 	} 
       
 	
@@ -25,26 +27,65 @@ public class DaoHome {
 		manager.close();
 	}
 	
-    public void listHome() {
-        List<Home> resultList = manager.createQuery("Select a From Person a", Home.class).getResultList();
+	
+	
+	/************************CRUD*************************/
+	/************************CRUD*************************/
+	/************************CRUD*************************/
+	
+	/**
+	 * Crée une Home dans la base de données
+	 */
+	public void createHome(Home h){
+		transaction.begin();
+		manager.persist(h);
+		transaction.commit();
+	}
+	
+	/**
+	 * Afficher les données relatives à toutes les Homes
+	 */
+    public void showHomes() {
+        List<Home> resultList = manager.createQuery("Select a From Home a", Home.class).getResultList();
         
         for (Home home : resultList) {
             System.out.println(home.toString());
         }
     }
     
-   public void listHomes() {
-	CriteriaBuilder criteriaBuilder = manager.getCriteriaBuilder();
-	CriteriaQuery<Home> query = criteriaBuilder.createQuery(Home.class);
-	Root<Home> from = query.from(Home.class);
-	/* for(Person p: from) {
-		 System.out.println(p.toString());
-	 }
-	//query.select(from.get("a"))
-	     .where(from.get("a").in(1, 2, 3, 4));*/
+    /**
+     * Reccupère la liste des Homes de la base de données
+     * @return la liste des Homes
+     */
+   public List<Home> getHomes() {
+
+	   CriteriaBuilder criteriaBuilder = manager.getCriteriaBuilder();
+		CriteriaQuery<Home> criteriaQuery = criteriaBuilder.createQuery(Home.class);
+		Root<Home> from = criteriaQuery.from(Home.class);
+		criteriaQuery.select(from);
+		TypedQuery<Home> query = manager.createQuery(criteriaQuery);
+		List<Home> homes = query.getResultList();
+		
+		return homes;
    }
    
    
+   /**
+    * Recherche une Home ayant pour identifiant id
+    * @param id : identification de la Home à reccuperer
+    * @return la Home ayant pour identifiant id
+    */
+   public Home getHome(int id)
+   {
+	  Long identifiant = new Long(id);
+	  return manager.find(Home.class, identifiant);
+	   
+   }
+   
+   /**
+    * Mis à jour la Home dans la base de données
+    * @param home : nouvelle données
+    */
    public void update(Home home)
    {
 	   manager.merge(home);
@@ -52,7 +93,10 @@ public class DaoHome {
 
 
 
-	
+   /**
+    * Supprime la Home home
+    * @param home : Home à supprimer
+    */
 	public void delete(Home home)
 	{
 	   manager.remove(home);
