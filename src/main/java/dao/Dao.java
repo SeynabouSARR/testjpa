afd3a37e77ba9988ee125876f256f7479c55ec06
Seynabou SARR <seynabousarr90@yahoo.fr>
2018-02-28 21:57:56 +0100

    exigences checkstyle

--- src/main/java/dao/Dao.java
+++ src/main/java/dao/Dao.java
@@ -8,28 +8,57 @@ import javax.persistence.Persistence;
  * @author Seynabou SARR et Melaine BOUE
  */
 public class Dao {
-	EntityManagerFactory factory;           
-    EntityManager manager;
-    
-	public Dao() {
-		
-	} 	
-	public EntityManager getManager() {
-		//on initialise le manager si c'est pas déjà fait
-		if(this.manager==null)
-		{
-			this.factory = Persistence.createEntityManagerFactory("mysql");
-			this.manager =  factory.createEntityManager();
-		}		
-		return manager;
-	}
-	
-	/**
-	 * Fermer la connexion
-	 */
-	public void close() {
-		manager.close();
-		factory.close();
-	}
+/**
+ * g�re la connexion persist.
+ */
+ private EntityManagerFactory factory;
+ /**
+  * le manager.
+  */
+ private EntityManager manager;
+ /**
+  * Constructor.
+  */
+  public Dao() { }
+/**
+ * getteur de la connexion.
+ * @return factory
+*/
+  public final EntityManagerFactory getFactory() {
+  return factory;
+  }
+/**
+ * modifier la connexion.
+ * @param factory
+*/
+  public final void setFactory(EntityManagerFactory factory) {
+  this.factory = factory;
+  }
+/**
+ * modifier la connexion.
+ * @param manager
+*/
+  public final void setManager(EntityManager manager) {
+  this.manager = manager;
+  }
+/**
+ * getteur de la connexion.
+  * @return manager
+*/
+  public final EntityManager getManager() {
+		//on initialise le manager si c'est pas déjà fait.
+  if(this.manager==null){
+	  this.factory = Persistence.createEntityManagerFactory("mysql");
+	  this.manager =  factory.createEntityManager();
+	  }
+  return manager;
+  }
+/**
+* Fermer la connexion.
+*/
+  public void close() {
+	 manager.close();
+	 factory.close();
+	 }
 }
 
